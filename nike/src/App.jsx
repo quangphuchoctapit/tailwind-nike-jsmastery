7cc1c24333d66c254d3cf07b03456eae3541ffc3
Fuc le <quangphuchoctapit@gmail.com>
2023-10-07 12:19:08 +0700

    build sketch of nike web interface

--- nike/src/App.jsx
+++ nike/src/App.jsx
@@ -1,7 +1,21 @@
-export default function App() {
+const App = () => {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello tailwind
-    </h1>
+    <main>
+      <div className='relative'>
+        Nav
+        <section className='xl:padding-1 wide:padding-r padding-b'>
+          hero
+        </section>
+        <section className="padding">PopularProducts</section>
+        <section className="padding">SuperQuality</section>
+        <section className="padding padding-x py-10">Services</section>
+        <section className="padding">SpecialOffer</section>
+        <section className="padding bg-pale-blue">CustomerReviews</section>
+        <section className="padding padding-x sm:py-32 py-16 w-full">Subscribe</section>
+        <section className="padding bg-black text-white padding-t pb-8">Footer</section>
+
+      </div>
+    </main>
   )
 }
+export default App
